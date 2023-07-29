54659a204dba5377bace87e17d2cd3ebc3d9cf02
Stephen Samuel <stephensamuelchung16@gmail.com>
2023-07-29 02:45:26 +0000

    Portfolio detail update

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,7 +24,7 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a Computer Science Student with passion for creating web apps using Python(Django). I am also a Microsoft Learn Student Ambassador. I enjoy tutoring and learning new stuff especially relating to technology and I am also a good cook. ";
 
 /**
  * List of some of skills or technologies you work on, are learning,
@@ -33,9 +33,9 @@ const description =
 const skillsList = [
   "Web design",
   "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
+  "Power Apps",
+  "Python",
+  "Excel Analytics",
   "Graphic design",
 ];
 
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my backend experience I continually look for new and better ways to make tech accessible by all.";
 
 const About = () => {
   return (
